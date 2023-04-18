a30c9ad25ee012c587f4a7a7c3479a6cd230ef84
Erul - Data Enthusiast <119069839+syaerulid@users.noreply.github.com>
2023-04-18 22:42:25 +0700

    Update Upscale Test.sql

--- Upscale Test.sql
+++ Upscale Test.sql
@@ -2,12 +2,10 @@ USE UPSCALE;
 
 SELECT *
 FROM TEST1;
-
 -- 1.RATA-RATA USIA
 SELECT 
 	ROUND(AVG (Age)) as Rata_Rata_Usia
 FROM TEST1; 
-
 -- 2. MEDIAN
 -- 2.1 SORTED
 -- COUNT
@@ -15,19 +13,16 @@ SELECT
     *
 FROM TEST1
 ORDER BY Income;
-
 -- MEDIAN
 SELECT
 	COUNT(ID) / 2 as Median
 FROM TEST1
 ORDER BY Income;
-
 -- MEDIAN IS
 SELECT
 Income as Median
 FROM TEST1
 WHERE Income = 75000
-
 -- 3. Persentase Laki-Laki
 SELECT
 	COUNT(*)
@@ -38,19 +33,16 @@ SELECT
 	COUNT(*) / 10 * 100 as Percentage_Male
 FROM TEST1
 WHERE Gender = 'Male';
-
 -- 4. Percentage Master
 SELECT
 	COUNT(*)
 FROM TEST1
 WHERE Education = 'Master';
-
 -- 4.1
 SELECT
 	COUNT(*) / 10 * 100 as Percentage_Master
 FROM TEST1
 WHERE Education = 'Master';
-
 -- 5. Rata-Rata usia orang PhD
 SELECT
 	ROUND(AVG(Age)) as Rata_PhD
